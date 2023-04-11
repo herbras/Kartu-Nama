f5f475611c784eb3072214250fbe621a83d76a90
Sarbeh <handoyonoterakhir@gmail.com>
2023-04-12 05:13:39 +0700

    yok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+import partytown from "@astrojs/partytown";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), partytown(), compressor()]
 });
