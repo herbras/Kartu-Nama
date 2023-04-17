a2fea7dbef993df832f1d514f7d640b7164c2819
Sarbeh <handoyonoterakhir@gmail.com>
2023-04-17 12:27:18 +0700

    ad

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import partytown from "@astrojs/partytown";
-
 import compressor from "astro-compressor";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), partytown(), compressor()]
+  integrations: [tailwind(), alpinejs(), partytown(), compressor()],
+  output: "server",
+  adapter: vercel()
 });
